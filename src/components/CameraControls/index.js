23bc1c008fecb8b20bffb24c887e0f9f316e4903
Oleh Parfeniuk <oleh.parfeniuk@artpix3d.com>
2021-01-21 15:26:21 +0200

    Customize camera

--- src/components/CameraControls/index.js
+++ src/components/CameraControls/index.js
@@ -6,19 +6,20 @@ const CameraControls = () => {
         camera,
         gl: {domElement}
     } = useThree();
+
     useFrame(() => controls.current.update());
     const controls = useRef();
-    camera.position.z = 10;
+    camera.position.z = 0;
 
     return (
         <orbitControls
             ref={controls}
             args={[camera, domElement]}
             enableZoom={true}
-            maxAzimuthAngle={Math.PI}
-            maxPolarAngle={Math.PI}
-            minAzimuthAngle={-Math.PI}
+            maxPolarAngle={Math.PI * 0.5}
             minPolarAngle={0}
+            minDistance={80}
+            maxDistance={200}
         />
     );
 };
--- src/components/Portrait/index.js
+++ src/components/Portrait/index.js
@@ -1,13 +1,17 @@
 import React, {useRef} from "react";
-import {useLoader} from "react-three-fiber";
+import {useFrame, useLoader, useThree} from "react-three-fiber";
 import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader";
 import portraitObj from "../../assets/582706_rendering.obj";
 import * as THREE from "three";
 
 const Portrait = () => {
     const group = useRef(null);
-
     const portrait = useLoader(OBJLoader, portraitObj);
+    const {camera} = useThree();
+    camera.position.z = 120;
+    camera.near = 100;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    console.log(camera)
 
     const material = new THREE.MeshBasicMaterial({
         side: THREE.DoubleSide,
